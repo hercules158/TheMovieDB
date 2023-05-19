544d66d85189e756cc685eab143bf4ede71bd870
Hércules Corcini <77862530+hercules158@users.noreply.github.com>
2023-05-19 18:41:38 -0300

    Update base_url.dart
    Omited my api key

--- lib/const/base_url.dart
+++ lib/const/base_url.dart
@@ -1,6 +1,6 @@
 class BaseURL {
   static const urlPopular =
-      'https://api.themoviedb.org/3/movie/popular?api_key=cfc0ade742438ee84c9021437abb434c';
+      'https://api.themoviedb.org/3/movie/popular?api_key=YourApiKey';
   static const urlSearch =
-      'https://api.themoviedb.org/3/search/movie?api_key=cfc0ade742438ee84c9021437abb434c';
+      'https://api.themoviedb.org/3/search/movie?api_key=YourApiKey';
 }
