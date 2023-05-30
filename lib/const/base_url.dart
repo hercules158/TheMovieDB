3dbf1dbcfca27a137225ce51e2e79fca6a10dfc7
Hércules Corcini <77862530+hercules158@users.noreply.github.com>
2023-05-29 22:46:05 -0300

    Update base_url.dart
    Removed my API_Key

--- lib/const/base_url.dart
+++ lib/const/base_url.dart
@@ -1,9 +1,9 @@
 class BaseURL {
   static const urlPopular =
-      'https://api.themoviedb.org/3/movie/popular?include_adult=false&api_key=cfc0ade742438ee84c9021437abb434c';
+      'https://api.themoviedb.org/3/movie/popular?include_adult=false&api_key=xxxx';
   static const urlTrendingWeekMix =
-      'https://api.themoviedb.org/3/trending/all/week?api_key=cfc0ade742438ee84c9021437abb434c';
-static const urlGenreHot = "https://api.themoviedb.org/3/discover/movie?include_adult=false&api_key=cfc0ade742438ee84c9021437abb434c&with_genres=";
+      'https://api.themoviedb.org/3/trending/all/week?api_key=xxxx';
+static const urlGenreHot = "https://api.themoviedb.org/3/discover/movie?include_adult=false&api_key=xxxx&with_genres=";
   static const urlSearch =
-      'https://api.themoviedb.org/3/search/multi?api_key=cfc0ade742438ee84c9021437abb434c';
+      'https://api.themoviedb.org/3/search/multi?api_key=xxxx';
 }
